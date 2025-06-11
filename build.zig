668dc44ac5dec178891b923133f7b2c543f0b5da
Edward Smale <essmale2005@gmail.com>
2025-06-11 01:57:39 +0100

    Add generate docs step

--- build.zig
+++ build.zig
@@ -17,4 +17,13 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const emitted_docs = exe_unit_tests.getEmittedDocs();
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = emitted_docs,
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const doc_step = b.step("docs", "Generate docs");
+    doc_step.dependOn(&install_docs.step);
 }
