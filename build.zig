b1fd9570fde7b62f7a146194be47d2a2f6f15fc4
Aurélien Cibrario <aurelien.cibrario@gmail.com>
2025-06-11 09:34:39 +0200

    docs : added doc step and basic custom html.
    Also added a cute logo. Manually svg authored based on a Gemini
    proposition.

--- build.zig
+++ build.zig
@@ -4,26 +4,34 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addModule("dishwasher", .{
+    const dishwasher = b.addModule("dishwasher", .{
         .root_source_file = b.path("src/dishwasher.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe,
+    // Doc
+    const docs = b.addLibrary(.{ .name = "dishwasher", .root_module = dishwasher });
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = docs.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+        // Seems a bit drastic but by default only index.html is installed
+        // and I override it below. Maybe there is a cleaner way ?
+        .exclude_extensions = &.{".html"},
     });
+    const install_html = b.addInstallFile(b.path("docs/index.html"), "docs/index.html");
+    const install_logo = b.addInstallFile(b.path("docs/logo.svg"), "docs/logo.svg");
+    const docs_step = b.step("docs", "Build documentation");
+    docs_step.dependOn(&install_docs.step);
+    docs_step.dependOn(&install_html.step);
+    docs_step.dependOn(&install_logo.step);
 
+    // Unit Tests
+    const exe_unit_tests = b.addTest(.{
+        .root_module = dishwasher,
+    });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
-
-    const emitted_docs = exe_unit_tests.getEmittedDocs();
-    const install_docs = b.addInstallDirectory(.{
-        .source_dir = emitted_docs,
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-    const doc_step = b.step("docs", "Generate docs");
-    doc_step.dependOn(&install_docs.step);
 }
