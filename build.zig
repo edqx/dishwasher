7fce2d32558dbe207b4508a6f16d5a71d446878b
Edward Smale <essmale2005@gmail.com>
2025-06-11 18:03:40 +0100

    Use custom index.html, improve docs generation by using library with proper name
    Co-authored-by: Aurélien Cibrario <aurelien.cibrario@gmail.com>

--- build.zig
+++ build.zig
@@ -4,26 +4,35 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addModule("dishwasher", .{
+    const dishwasher = b.addModule("dishwasher", .{
         .root_source_file = b.path("src/dishwasher.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe,
+    const dishwasher_tests = b.addTest(.{
+        .root_module = dishwasher,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_dishwasher_tests = b.addRunArtifact(dishwasher_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_dishwasher_tests.step);
 
-    const emitted_docs = exe_unit_tests.getEmittedDocs();
+    const docs_lib = b.addLibrary(.{
+        .name = "dishwasher",
+        .root_module = dishwasher,
+    });
+
     const install_docs = b.addInstallDirectory(.{
-        .source_dir = emitted_docs,
+        .source_dir = docs_lib.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
+        .exclude_extensions = &.{".html"},
     });
-    const doc_step = b.step("docs", "Generate docs");
-    doc_step.dependOn(&install_docs.step);
+
+    const install_html = b.addInstallFile(b.path("docs/index.html"), "docs/index.html");
+
+    const docs_step = b.step("docs", "Generate docs");
+    docs_step.dependOn(&install_docs.step);
+    docs_step.dependOn(&install_html.step);
 }
